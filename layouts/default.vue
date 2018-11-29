e89fccf125d9dc24dcd0556c52bddb4920aa2891
MaeglynD <31933322+MaeglynD@users.noreply.github.com>
2018-11-29 06:33:02 +0000

    init

---
+++ layouts/default.vue
@@ -0,0 +1,496 @@
+<template>
+  <v-app dark>
+    <v-toolbar clipped-left id="toolbar"  flat fixed :class="{'toolbar-scroll': scrollPosition}">
+      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title><router-link to="/">papes<span class="glow">.</span>io</router-link></v-toolbar-title>
+
+      <v-card class="input-card hidden-xs-only" hover flat color="rgba(255, 255, 255, 0.15)" height="40px" >
+        <v-text-field placeholder="Search" id="search-bar" ref="searchBar" height="40px" class="ma-0 pa-0"> </v-text-field>
+      </v-card>
+      <v-spacer></v-spacer>
+
+      <v-dialog v-model="dialog" class="dialog-container" height="675px" width="450px" lazy transition="slide-y-transition">
+      <v-btn dark color="rgba(255, 255, 255, 0)" large depressed class="toolbtn" slot="activator" @click="changeDiaClassLogin()"><span class="hidden-lg-and-up"><span class="mdi mdi-login"></span></span><span class="hidden-md-and-down">sign in</span></v-btn>
+      <v-btn dark color="rgba(255, 255, 255, 0)" large depressed class="toolbtn create-acc" slot="activator" @click="changeDiaClassCreate()"><span class="hidden-lg-and-up"><span class="mdi mdi-account-plus"></span></span><span class="hidden-md-and-down">create account</span></v-btn>
+
+      <v-card class="dialog-account">
+        <v-card class="dialog-top" color="rgba(0,0,0,0)" flat >
+          <v-layout row wrap>
+            <v-flex xs1>
+              <v-btn icon large  class="title " @click="dialog = false"><v-icon>close</v-icon></v-btn>
+            </v-flex>
+            <v-flex xs10>
+              <v-card-text center class="tab-title">
+                {{ tabText }}
+              </v-card-text>
+            </v-flex>
+          </v-layout>
+        </v-card>
+        <v-layout row wrap class="px-4">
+          <v-flex>
+            <v-tabs v-model="tabs" color="transparent" slider-color="primary" centered grow slot="extension" >
+              <v-tabs-slider color="primary"></v-tabs-slider>
+              <v-tab href="#CREATE" @click="tabText = 'Create Account'">
+                <span class="mdi mdi-account-plus"></span>
+              </v-tab>
+              <v-tab href="#LOGIN" @click="tabText = 'Sign In'">
+                <span class="mdi mdi-login"></span>
+              </v-tab>
+              <v-tab href="#FORGOT" @click="tabText = 'Reset Password'">
+                <span class="mdi mdi-key-variant"></span>
+              </v-tab>
+            </v-tabs>
+          </v-flex>
+          <v-tabs-items v-model="tabs">
+            <v-tab-item value="CREATE">
+              <v-card class="tab-item-container" color="transparent" flat>
+              <v-text-field placeholder="Username" class="ma-0 pa-0" type="text" hint="3 or more characters" required> </v-text-field>
+              <v-text-field placeholder="Email" class="ma-0 pa-0 my-1" type="email" hint="6 or more characters" required> </v-text-field>
+              <v-text-field placeholder="Password" class="ma-0 pa-0" :type="show1 ? 'text' : 'password'" hint="6 or more characters" required :append-icon="show1 ? 'visibility_off' : 'visibility'" @click:append="show1 = !show1"> </v-text-field>
+              <v-text-field placeholder="Confirm Password" class="ma-0 pa-0 my-1" :type="show2 ? 'text' : 'password'" hint="6 or more characters" required :append-icon="show2 ? 'visibility_off' : 'visibility'" @click:append="show2 = !show2"> </v-text-field>
+              <v-btn color="primary" hover class="create-acc-btn mt-1" >Create Account</v-btn>
+              </v-card>
+            </v-tab-item>
+            <v-tab-item value="LOGIN">
+              <v-card class="tab-item-container" color="transparent" flat>
+              <v-text-field placeholder="Email" class="ma-0 pa-0" type="email" hint="6 or more characters" required> </v-text-field>
+              <v-text-field placeholder="Password" class="ma-0 pa-0 my-2" :type="show2 ? 'text' : 'password'" hint="6 or more characters" required :append-icon="show3 ? 'visibility_off' : 'visibility'" @click:append="show3 = !show3"> </v-text-field>
+              <v-btn color="primary" hover>Sign In</v-btn>
+              <v-btn color="primary" hover flat>FORGOT PASSWORD?</v-btn>
+              </v-card>
+            </v-tab-item>
+            <v-tab-item value="FORGOT">
+              <v-card class="tab-item-container" color="transparent" flat>
+              <v-text-field placeholder="Email" class="ma-0 pa-0 mb-2" type="email" hint="6 or more characters" required> </v-text-field>
+              <v-btn color="primary" hover>Send confirmation email</v-btn>
+              </v-card>
+            </v-tab-item>
+          </v-tabs-items>
+        </v-layout>
+      </v-card>
+      </v-dialog>
+    </v-toolbar>
+    <v-navigation-drawer v-model="drawer" fixed clipped id="navigation-drawer" :width="250" temporary>
+      <div class="scroll-fix"></div>
+      <v-list id="drawer-list" data-simplebar="init">
+        <v-divider></v-divider>
+        <v-list-tile to="/" ripple active-class="primary--text" active class="pt-2">
+          <v-list-tile-action class="pl-2 " >
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content class="pl-2">
+            Home
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile to="/trending" ripple>
+          <v-list-tile-action class="pl-2">
+            <span class="mdi mdi-fire"></span>
+          </v-list-tile-action>
+          <v-list-tile-content class="pl-2">
+            Trending
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile  to="/categories" ripple>
+          <v-list-tile-action class="pl-2" >
+            <span class="mdi mdi-apps"></span>
+          </v-list-tile-action>
+          <v-list-tile-content class="pl-2">
+            Categories
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile  @click="searchFocus()" ripple class="pb-2">
+          <v-list-tile-action class="pl-2" >
+            <span class="mdi mdi-magnify"></span>
+          </v-list-tile-action>
+          <v-list-tile-content class="pl-2">
+            Search
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-divider></v-divider>
+        <v-subheader class="subheading grey--text text--darken-1">ACCOUNT</v-subheader>
+        <v-list-tile   @click="changeDiaClassLoginDrawer()" ripple>
+          <v-list-tile-action class="pl-2" >
+            <span class="mdi mdi-login"></span>
+          </v-list-tile-action>
+          <v-list-tile-content class="pl-2">
+            Sign In
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile  @click="changeDiaClassCreateDrawer()" ripple class="pb-2">
+          <v-list-tile-action class="pl-2" >
+            <span class="mdi mdi-account-plus"></span>
+          </v-list-tile-action>
+          <v-list-tile-content class="pl-2">
+            Create Account
+          </v-list-tile-content>
+        </v-list-tile>
+
+        <v-divider></v-divider>
+        <v-subheader class="subheading grey--text text--darken-1">CONTACT</v-subheader>
+
+        <v-list-tile @click="contactDialogChange()" ripple>
+          <v-list-tile-action class="pl-2" >
+            <span class="mdi mdi-message-alert"></span>
+          </v-list-tile-action>
+          <v-list-tile-content class="pl-2">
+            Contact Us
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile  href="https://discordapp.com/" target="_blank" ripple>
+          <v-list-tile-action class="pl-2" >
+            <span class="mdi mdi-discord"></span>
+          </v-list-tile-action>
+          <v-list-tile-content class="pl-2">
+            Discord
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile  href="https://twitter.com/papes_io" target="_blank" ripple>
+          <v-list-tile-action class="pl-2" >
+            <span class="mdi mdi-twitter"></span>
+          </v-list-tile-action>
+          <v-list-tile-content class="pl-2">
+            Twitter
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-dialog v-model="contactDialog" class="dialog-container" height="675px" width="450px" lazy transition="slide-y-transition">
+      <v-card class="dialog-contact">
+        <v-card class="dialog-top" color="rgba(0,0,0,0)" flat >
+          <v-layout row wrap>
+            <v-flex xs1>
+              <v-btn icon large  class="title " @click="contactDialog = false"><v-icon>close</v-icon></v-btn>
+            </v-flex>
+            <v-flex xs10>
+              <v-card-text center class="tab-title">
+                Contact Us
+              </v-card-text>
+            </v-flex>
+          </v-layout>
+          <v-card class="tab-item-container" color="transparent" flat>
+          <v-text-field placeholder="Name" class="ma-0 pa-0 pb-1" type="text" required> </v-text-field>
+          <v-text-field placeholder="Email" class="ma-0 pa-0 my-1 pb-1" type="email" hint="6 or more characters" required> </v-text-field>
+          <v-textarea placeholder="Message" class="ma-0 pa-0 pb-1" hint="500 characters or less" required maxlength="500"> </v-textarea>
+          <v-btn color="primary" hover class="create-acc-btn mt-1" >Send</v-btn>
+          </v-card>
+        </v-card>
+      </v-card>
+    </v-dialog>
+    <v-slide-y-transition mode="out-in">
+      <nuxt />
+    </v-slide-y-transition>
+    <div class="main-footer-container">
+      <div class="main-footer-text">
+        <v-container grid-list-xs,sm,md,lg,xl>
+          <v-layout row wrap>
+            <v-flex xs6 sm3 class="pl-5">
+              <h4 class="mt-5 mb-3">Directory</h4>
+              <a href="#"><p>Home</p></a>
+              <a href="#"><p>Browse</p></a>
+              <a href="#"><p>Random</p></a>
+              <a href="#"><p>Search</p></a>
+              <a href="#"><p>Recent</p></a>
+            </v-flex>
+            <v-flex xs6 sm3 class="pl-5">
+              <h4 class="mt-5 mb-3">Business</h4>
+              <a href="#"><p>Staff</p></a>
+              <a href="#"><p>What's New</p></a>
+              <a href="#"><p>Upcoming</p></a>
+            </v-flex>
+            <v-flex xs6 sm3 class="pl-5">
+              <h4 class="mt-5 mb-3">Resources</h4>
+              <a href="#"><p>Cookie Policy</p></a>
+              <a href="#"><p>Advertising</p></a>
+            </v-flex>
+            <v-flex xs6 sm3 class="pl-5">
+              <h4 class="mt-5 mb-3">Connect</h4>
+              <a href="#"><p>Conact Us</p></a>
+              <a href="#"><p>Discord</p></a>
+              <a href="#"><p>Facebook</p></a>
+              <a href="#"><p>Twitter</p></a>
+              <a href="#"><p>Youtube</p></a>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </div>
+    </div>
+    <div class="main-footer-io"><p>papes<span class="glow">.</span>io</p></div>
+  </v-app>
+</template>
+
+<script>
+
+
+
+  export default {
+
+    data () {
+      return {
+        drawer: false,
+        dialog: false,
+        toolActive: false,
+        tabs: "FORGOT",
+        show1: false,
+        show2: false,
+        show3: false,
+        tabText: "",
+        scrollPosition: null,
+        title: 'Vuetify.js',
+        contactDialog: false
+      }
+    },
+    methods: {
+      changeDiaClassLogin(){
+        this.tabs = "LOGIN"
+        this.tabText = "Sign In"
+      },
+      changeDiaClassCreate(){
+        this.tabs = "CREATE"
+        this.tabText = "Create Account"
+      },
+      changeDiaClassCreateDrawer(){
+        this.drawer = false
+        this.dialog = true
+        this.tabs = "CREATE"
+        this.tabText = "Create Account"
+      },
+      changeDiaClassLoginDrawer(){
+        this.drawer = false
+        this.dialog = true
+        this.tabs = "LOGIN"
+        this.tabText = "Sign In"
+      },
+      updateScroll() {
+        this.scrollPosition = window.scrollY
+      },
+      searchFocus(){
+        this.drawer = false
+        this.$refs.searchBar.focus()
+      },
+      contactDialogChange(){
+        this.drawer = false
+        this.contactDialog = true
+      }
+    },
+    mounted() {
+     window.addEventListener('scroll', this.updateScroll);
+   }
+  }
+</script>
+
+<style>
+body{
+background: rgb(23,24,26);
+}
+.theme--dark.application {
+  background: transparent;
+}
+a:link {
+  color: inherit;
+  text-decoration: none;
+}
+a:visited {
+  color: inherit;
+}
+a:hover {
+  color: inherit;
+}
+a:active {
+  color: inherit;
+}
+.glow{
+color: #f3c669;
+text-shadow: 0 0 1px #f3c669;
+}
+
+#toolbar{
+z-index: 100;
+background-color: rgba(0,0,0,0);
+}
+#toolbar.toolbar-scroll{
+background-color: rgba(15,15,15, 0.99);
+
+}
+#navigation-drawer{
+background: rgb(52, 52, 52);
+}
+.v-navigation-drawer__border{
+display: none;
+}
+#drawer-list{
+padding-top: 67px;
+}
+.mdi{
+font-size: 24px;
+}
+.drawer-active{
+color: #f3c669;
+}
+.input-card{
+height: 70%;
+width: 30vw;
+min-width: 200px;
+position: absolute;
+left: 50%;
+margin-left: -15vw;
+}
+#search-bar{
+text-align:center;
+}
+#search-bar::placeholder{
+text-align:left;
+color: rgba(255, 255, 255, 0.4);
+padding-left: 14px;
+}
+.input-card .v-text-field>.v-input__control>.v-input__slot:before{
+border: 0;
+}
+.toolbtn{
+font-size: 17.5px;
+}
+.scroll-fix{
+position: fixed;
+top: 0;
+left: 0;
+width: 100%;
+z-index: 101;
+height: 80px;
+background: -webkit-linear-gradient(180deg,#303030 0,#303030 60%,rgba(48,48,48,0) 100%);
+background: -moz-linear-gradient(180deg,#303030 0,#303030 60%,rgba(48,48,48,0) 100%);
+background: -ms-linear-gradient(180deg,#303030 0,#303030 60%,rgba(48,48,48,0) 100%);
+background: -o-linear-gradient(180deg,#303030 0,#303030 60%,rgba(48,48,48,0) 100%);
+background: linear-gradient(180deg,#303030 0,#303030 60%,rgba(48,48,48,0) 100%);
+}
+.v-dialog{
+overflow: hidden;
+margin: 0;
+}
+.dialog-account{
+background: url("../static/dialog-pic.png") top center/cover no-repeat;
+width: 450px;
+height: 675px;
+border: 1px solid rgba(255, 255, 255, 0.6);
+}
+.dialog-contact{
+background: url("../static/contact-pic.png") top center/cover no-repeat;
+width: 450px;
+height: 675px;
+border: 1px solid rgba(255, 255, 255, 0.6);
+}
+.dialog-contact textarea{
+  resize: none;
+  height: 225px;
+}
+.dialog-top{
+height: 185px;
+width: 100%;
+}
+
+.tab-item-container{
+text-align: center;
+width: 400px;
+border-radius: 0;
+margin: auto;
+padding-top: 30px;
+}
+.tab-item-container .v-text-field>.v-input__control>.v-input__slot{
+padding: 10px 24px;
+background: rgba(10,10,10,0.7);
+transition: 0.2s;
+border-bottom: 1px solid rgba(255, 255, 255, 0);
+}
+.tab-item-container .v-text-field>.v-input__control>.v-input__slot:hover{
+  border-bottom: 1px solid rgba(255, 255, 255, 0.1);
+}
+.tab-item-container .v-text-field>.v-input__control>.v-input__slot:before{
+border: 0;
+}
+.tab-item-container .v-btn{
+width: 100%;
+margin: 0;
+height: 44px;
+margin-bottom: 10px;
+}
+.tab-item-container .create-acc-btn{
+  height: 50px;
+}
+
+
+.tab-title{
+font-size: 22px;
+text-shadow: 0 0 1px darkgrey;
+text-align: center;
+}
+.main-footer-container{
+  background: #0a0a0b;
+  width: 100%;
+  padding-bottom: 100px;
+}
+.main-footer-text{
+
+  margin: auto;
+  max-width: 1100px;
+  font-size: 15px;
+}
+.main-footer-text p{
+  font-weight: 300;
+  opacity: 0.7;
+  transition: 0.2s;
+}
+.main-footer-text p:hover{
+  opacity: 1;
+}
+.main-footer-io{
+  position: relative;
+  bottom: 0;
+  text-align: center;
+  padding-bottom: 20px;
+  padding-top: 30px;
+  font-size: 20px;
+  width: 100%;
+  background: #0d0e0f;
+}
+@media only screen and (max-width: 770px) {
+.toolbtn{
+  padding: 0;
+  margin: 0;
+}
+}
+@media only screen and (max-width: 450px) {
+.create-acc{
+ display: none;
+}
+}
+@media only screen and (max-width: 530px), screen and (max-height: 710px) {
+.v-dialog{
+  box-shadow: none;
+}
+.dialog-account, .dialog-contact{
+  margin: auto;
+  width: 305px;
+  height: 420px;
+}
+.dialog-contact textarea{
+  resize: none;
+  height: 110px;
+}
+.dialog-top{
+  height: 40px;
+  width: 100%;
+}
+.tab-item-container{
+  width: 255px;
+  padding-top: 10px;
+}
+.tab-item-container .v-text-field>.v-input__control>.v-input__slot{
+  padding: 2px 24px;
+}
+.tab-title{
+  font-size: 17px;
+}
+.mdi-key-variant, .mdi-login, .mdi-account-plus{
+  font-size: 18px;
+}
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,308 @@
+<template>
+  <div>
+    <div class="main-home-container">
+      <v-container grid-list-xs,sm,md,lg,xl class="main-v-cont" >
+        <v-layout align-center justify-center row fill-height>
+          <v-flex xs12 class="main-info">
+            <h1 class="">Cross platform, high definition papes from /<span class="glow">wg</span>/</h1>
+            <p class="mt-4">We centralize wallpapers of all shapes, sizes and contents so you can browse with <span class="glow">confidence</span>. Anime, nature, winter, summer - you name it we‘ve got it.</p>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </div>
+    <v-layout class="main-gradient-container" align-center justify-center row fill-height >
+      <span class="mdi mdi-chevron-down  display-1 mt-5" @click="$vuetify.goTo('.main-category-container')"></span>
+    </v-layout>
+    <div class="main-category-container">
+      <v-container grid-list-xs,sm,md,lg,xl class="main-cat-v">
+        <h1 class="hidden-sm-and-up text-xs-center mb-4">Browse</h1>
+        <v-layout row class=" mb-5">
+          <v-flex xs6 hidden-xs-only  align-self-center>
+            <h1 class="pa-0 ma-0">Browse</h1>
+          </v-flex>
+          <v-flex xs12 sm6 class=" control-cont align-self-center">
+            <v-btn  class="pa-0 ma-0 right subheading" depressed outline>ALL</v-btn>
+            <div class="main-control-cont">
+              <v-btn  :class="{'pa-0': true, 'ma-0': true, 'right': true, 'ml-2': $vuetify.breakpoint.smAndUp, 'mr-4': $vuetify.breakpoint.smAndUp}" depressed ><v-icon>chevron_right</v-icon></v-btn>
+              <v-btn  class="pa-0 ma-0 right" disabled  ><v-icon>chevron_left</v-icon></v-btn>
+            </div>
+          </v-flex>
+        </v-layout>
+        <div class="main-choose-cont">
+          <v-layout align-space-around row justify-center :class="{'wrap': $vuetify.breakpoint.smAndDown, 'fill-height': $vuetify.breakpoint.mdAndUp }">
+            <v-flex xs12 sm12 m4 >
+              <div class="cat-parent">
+                <div class="cat-text">
+                  <h2>mood</h2>
+                  <span class="mdi mdi-weather-lightning"></span>
+                </div>
+                <div class="cat-child a">
+
+                </div>
+              </div>
+            </v-flex>
+            <v-flex xs12  sm12 m4 :class="{'mx-4': $vuetify.breakpoint.mdAndUp}">
+              <div class="cat-parent">
+                <div class="cat-text">
+                  <h2>trending</h2>
+                  <span class="mdi mdi-fire"></span>
+                </div>
+                <div class="cat-child b">
+
+                </div>
+              </div>
+            </v-flex>
+            <v-flex xs12 sm12 m4>
+              <div class="cat-parent">
+                <div class="cat-text">
+                  <h2>Seasonal</h2>
+                  <span class="mdi mdi-snowflake"></span>
+                </div>
+                <div class="cat-child c">
+
+                </div>
+              </div>
+            </v-flex>
+          </v-layout>
+        </div>
+      </v-container>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.main-home-container{
+  top: 0;
+  height: 100vh;
+  width: 100%;
+  background: url("../static/main-bg.png") top center/cover no-repeat;
+  max-height: 969px;
+  min-height: 500px;
+}
+.main-v-cont{
+   max-width: 900px;
+   height: 100%;
+
+}
+.main-info h1{
+  font-size: 60px;
+  line-height: 1.3;
+  font-weight: 300;
+  text-shadow: 3px 4px 5px rgba(0, 0, 0, 0.18);
+}
+.main-info p{
+  max-width: 830px;
+  line-height: 1.3;
+  font-size:31.5px;
+  font-weight: 300;
+  color: rgba(255, 255, 255, 0.7);
+}
+.main-info{
+  margin-top: -10%;
+}
+.main-gradient-container{
+  margin-top: -300px;
+  width: 100%;
+  height: 300px;
+  background: rgb(23,24,26); /* Old browsers */
+  background: -moz-linear-gradient(top, rgba(23,24,26,0) 0%, rgba(23,24,26,1) 100%); /* FF3.6-15 */
+  background: -webkit-linear-gradient(top, rgba(23,24,26,0) 0%,rgba(23,24,26,1) 100%); /* Chrome10-25,Safari5.1-6 */
+  background: linear-gradient(to bottom, rgba(23,24,26,0) 0%,rgba(23,24,26,1) 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#17181a', endColorstr='#17181a',GradientType=0 ); /* IE6-9 */
+}
+.main-gradient-container span:hover{
+  cursor: pointer;
+}
+.main-category-container{
+  height: 100vh;
+  min-height: 800px;
+  max-height: 969px;
+  width: 100%;
+  padding-top: 64px;
+  background: rgb(23,24,26);
+}
+.main-cat-v{
+  max-width: 1100px;
+  height: 100%;
+  padding-top: 64px;
+}
+.main-cat-v h1{
+  font-weight: 300;
+  font-size: 40px;
+}
+.main-control-cont .v-btn{
+  background-color: rgba(255, 255, 255, 0);
+  min-width: 40px;
+  border: 1px solid rgba(255, 255, 255, 0.14);
+}
+.main-control-cont .v-btn--disabled{
+  border: 0px;
+  background: #38393a;
+}
+.control-cont .v-btn{
+  height: 45px;
+}
+.main-choose-cont{
+  width: 100%;
+
+  height: 500px;
+}
+.cat-parent, .cat-child{
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  transition: 0.4s;
+}
+.cat-child{
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  filter: brightness(40%);
+  margin-top: -500px;
+}
+.cat-parent .a{
+  background-image: url("../static/cat-a.png");
+}
+.cat-parent .b{
+  background-image: url("../static/cat-b.png");
+}
+.cat-parent .c{
+  background-image: url("../static/cat-c.png");
+}
+.cat-text{
+  width: 100%;
+  height: 100%;
+  position: relative;
+  z-index: 1;
+  text-align: center;
+}
+.cat-text h2{
+  font-size: 27px;
+  font-weight: 300;
+  letter-spacing: 7px;
+  text-transform: uppercase;
+  padding-top: 20px;
+  padding-bottom: 40px;
+}
+.cat-text span{
+  font-size: 170px;
+}
+.cat-child{
+  border: 1px solid rgba(255, 255, 255, 0.4)
+}
+.cat-parent:hover{
+  cursor: pointer;
+}
+.cat-parent:hover .cat-child, .cat-parent:focus .cat-child{
+    transform: scale(1.2);
+    filter: brightness(100%);
+}
+@media only screen and (max-width: 1600px), screen and (max-height: 800px) {
+  .main-v-cont{
+     max-width: 720px;
+
+  }
+  .main-info h1{
+    font-size: 48px;
+
+  }
+  .main-info p{
+    max-width: 664px;
+    font-size:25.2px;
+  }
+}
+@media only screen and (max-height: 800px) {
+  .main-category-container{
+    min-height:740px;
+  }
+  .main-choose-cont{
+    height: 400px;
+  }
+  .cat-child{
+    margin-top: -400px;
+  }
+  .cat-text span{
+    font-size: 130px;
+  }
+}
+@media only screen and (max-width: 960px) {
+  .main-category-container{
+    height: 1300px;
+    max-height: 1300px;
+  }
+  .main-choose-cont{
+    height: 1200px;
+  }
+  .cat-child{
+    background-position: center center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    filter: brightness(40%);
+    margin-top: -300px;
+  }
+  .cat-parent{
+    height: 300px;
+  }
+  .cat-text{
+    width: 100%;
+    height: 100%;
+    position: relative;
+    z-index: 1;
+    text-align: center;
+  }
+  .cat-text h2{
+    font-size: 27px;
+    font-weight: 300;
+    letter-spacing: 7px;
+    text-transform: uppercase;
+    padding-top: 20px;
+    padding-bottom: 5px;
+  }
+  .cat-text span{
+    font-size: 130px;
+  }
+}
+@media only screen and (max-height: 700px) {
+  .main-info{
+    margin-top: 0%;
+  }
+}
+@media only screen and (max-width: 1000px), screen and (max-height: 600px) {
+  .main-home-container{
+    text-align: center;
+  }
+  .main-v-cont{
+     max-width: 90%;
+     padding: 0 20px;
+
+  }
+  .main-info p{
+    max-width: 100%;
+  }
+}
+@media only screen and (max-width: 700px) {
+  .main-info h1{
+    font-size: 30px;
+
+  }
+  .main-category-container h1{
+    font-size: 30px;
+  }
+  .main-info p{
+    font-size:18px;
+  }
+}
+@media only screen and (max-width: 600px) {
+  .main-control-cont .v-btn{
+    min-width: 33%;
+  }
+  .control-cont .v-btn{
+    width: 33%;
+  }
+}
+@media only screen and (max-width: 350px) {
+  .main-info p{
+    font-size:16px;
+  }
+}
+</style>
